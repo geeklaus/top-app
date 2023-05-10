e2e8db47e31e2c58972f5460de1471a9b0baeef3
geeklaus <grigoryantipin@gmail.com>
2023-05-10 14:39:17 +0300

    Implemented first simple component "Htag". Added google fonts. Switched language attribute of the page. Added main global variables for colors in global.css.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 
 # command to install stylelint dependency
 npm i -D --legacy-peer-deps stylelint stylelint-config-standard stylelint-order stylelint-order-config-standard
+
+# react main effects
+useState, useEffect, useReducer, useContext, useLayoutEffect
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { AppProps } from 'next/dist/shared/lib/router/router';	
 import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return <>
+		<Head>
+			<title>MyTop - наш лучший топ</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
 
 export default MyApp;
