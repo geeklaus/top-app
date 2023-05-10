2cde0234434b2db6b5d22619127ad6c78dcfa020
geeklaus <grigoryantipin@gmail.com>
2023-05-10 20:39:41 +0300

    Added @svgr/weback. Created P component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag='h1'>Текст</Htag>
-			<Button appearance='primary'>Кнопка</Button>
-			<Button appearance='ghost'>Кнопка</Button>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
 		</>
 	);
 }
