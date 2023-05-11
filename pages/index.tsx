fba6221ba2d6d5487c18db5236887506cdb4ecfd
geeklaus <grigoryantipin@gmail.com>
2023-05-11 13:05:48 +0300

    Created Rating component. Added eslint-plugin-react-hooks.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Button, Htag, P, Tag } from '../components';
+import React, { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
+
 	return (
 		<>
-			<Htag tag='h1'>Текст</Htag>
+			<Htag tag='h1'>Заголовок</Htag>
 			<Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
 			<P size='l'>Большой</P>
@@ -14,6 +16,7 @@ export default function Home(): JSX.Element {
 			<Tag size='m' color='red'>Red</Tag>
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Green</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	);
 }
