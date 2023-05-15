42cca32c7241cd35b30a25addc3a0909444f4e60
geeklaus <grigoryantipin@gmail.com>
2023-05-15 17:38:44 +0300

    Added convenient routing structure. Changed files structure.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/shared/lib/router/router';	
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 import Head from 'next/head';
 import React from 'react';
