7b0990d98b673a72f9269eafef28e71a6e97f5b5
geeklaus <grigoryantipin@gmail.com>
2023-05-17 14:56:15 +0300

    Added Input and Textarea components.

---
+++ components/Input/Input.props.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+
+export interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+
+}
--- components/index.ts
+++ components/index.ts
@@ -7,3 +7,5 @@ export * from './Card/Card';
 export * from './HhData/HhData';
 export * from './Advantages/Advantages';
 export * from './Sort/Sort';
+export * from './Input/Input';
+export * from './Textarea/Textarea';
